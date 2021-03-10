c08eefcd2aef9a1a11af6bb525c75f29712e8d13
kleidione Freitas <kleidione@gmail.com>
2021-03-09 22:37:59 -0300

    vendor: aospa: Added custom theme icons
    * Extracted from PixelLauncher module: https://t.me/modulesrepo/2440
    
    Signed-off-by: kleidione <kleidione@gmail.com>
    Change-Id: Ieb5d1965feaacea96f3229cc5aefd34ac683a743
    Signed-off-by: kleidione <kleidione@gmail.com>

--- target/product/packages.mk
+++ target/product/packages.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Inherit from rro_overlays config
+$(call inherit-product, vendor/aospa/config/rro_overlays.mk)
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
